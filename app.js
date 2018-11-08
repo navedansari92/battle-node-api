c6fdeb729382c831cf6ccc9ad6a060b0486feaf6
navedansari92 <36340338+navedansari92@users.noreply.github.com>
2018-11-09 01:02:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const router=express.Router();
 const bodyParser=require("body-parser");
-const port="3000";
+const port = process.env.port || 3000;
 const battleRoutes=require("./routes/battleRoutes");
 
 //app.use(router);
